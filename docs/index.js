08c9728e7e3ad570edf11722d4013832a1d4462f
Ebrahem Luhar <eb.luhar@gmail.com>
2019-04-24 20:06:30 +0100

    added docs folder

---
+++ docs/index.js
@@ -0,0 +1,7 @@
+function openSideMenu() {
+    document.getElementById('mobile-menu').style.transform = "translateX(0px)";
+}
+
+function closeSideMenu() {
+    document.getElementById('mobile-menu').style.transform = "translateX(-255px)";
+}
